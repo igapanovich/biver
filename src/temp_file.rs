9b72233be2b533600b862907ebb271e6ee2faf5b
Igor Gapanovich <igapanovich@proton.me>
2026-03-29 16:04:30 +0200

    Using xdg runtime dir for temp files if available

--- src/temp_file.rs
+++ src/temp_file.rs
@@ -1,8 +1,16 @@
+use std::env;
 use std::env::temp_dir;
 use std::path::PathBuf;
 use uuid::Uuid;
 
 pub fn path() -> PathBuf {
     let file_name = Uuid::new_v4().to_string();
-    temp_dir().join(file_name)
+
+    let temp_dir = if let Some(runtime_dir) = env::var_os("XDG_RUNTIME_DIR") {
+        PathBuf::from(runtime_dir)
+    } else {
+        temp_dir()
+    };
+
+    temp_dir.join("biver").join(file_name)
 }
